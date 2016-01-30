d356450cd3b1c2ead221861f506b5b4fd1d3972b
Comandeer <vepomoc@gmail.com>
2016-01-30 16:18:11 +0100

    Small refactoring of all.js.

--- rozwiazanie/js/all.js
+++ rozwiazanie/js/all.js
@@ -1,18 +1,13 @@
-$(function(){
-   $('form').submit(function(){
-      var $form = $(this);
-      var values = $form.find('input[name="character"]:checked').map(function(){
-          return this.value;
-      });
+$( function() {
+	var audio = new Audio('imperial_march.mp3');
 
-       var only_dark_side = _(values).every(function(value){
-           return value == 'dark_side';
-       });
+	$( 'form' ).submit( function( e ) {
+		var checked = $( ':checked', this );
 
-       if (values.length && only_dark_side){
-           var audio = new Audio('imperial_march.mp3');
-           audio.play();
-           return false;
-       }
-   });
-});
+		if ( checked.length && !checked.not( '[value=dark_side]' ).length ) {
+			audio.currentTime = 0;
+			audio.play();
+			e.preventDefault();
+		}
+	 } );
+} );
